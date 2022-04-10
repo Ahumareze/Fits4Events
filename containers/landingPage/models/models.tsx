831e33c0557176938930ae1c929dea8494185497
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-04-10 22:46:33 +0100

    April 10th commit

--- containers/landingPage/models/models.tsx
+++ containers/landingPage/models/models.tsx
@@ -5,11 +5,21 @@ import classes from './models.module.css';
 
 //imported utilities
 const skycrapper = require('../../../public/assets/skycrapper.png')
+const user = require('../../../public/assets/user1.jpg');
 
 const Post = () => {
     return (
         <div className={classes.post}>
             <div className={classes.image} style={{backgroundImage: `url(${skycrapper.default.src})`}} />
+            <p className={classes.title}>Plans for 5 star hotel in Lekki</p>
+            <div className={classes.userSection}>
+                <div className={classes.userImage} style={{backgroundImage: `url(${user.default.src})`}} />
+                <div className={classes.userDetails}>
+                    <div className={classes.userName}>
+                        <p>Username</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
