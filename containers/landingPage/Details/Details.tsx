a0164f73a51f11bc8db997a069cd73a8b9e209b5
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-04-27 04:18:52 +0100

    installed aos

--- containers/landingPage/Details/Details.tsx
+++ containers/landingPage/Details/Details.tsx
@@ -8,7 +8,7 @@ import { RedBall } from '../../../components';
 
 function Details() {
     return (
-        <div className={classes.container}>
+        <div className={classes.container} data-aos="fade-up">
             <div className={classes.container1}>
                 <div className={classes.canva}>
                     <RedBall />
@@ -17,7 +17,7 @@ function Details() {
             <div className={classes.container2}>
                 <h1>Access to Ideas and Projects all Around the Globe</h1>
                 <p>Why limit yourself to ideas investment opportunities within your vicinity, Logo provides access to ideas and investors around the world.</p>
-                <div>Get Started</div>
+                <div data-aos="fade-up">Get Started</div>
             </div>
         </div>
     );
