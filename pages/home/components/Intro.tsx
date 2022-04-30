8fc9e8a04325fee773cc8f94e0188d6eacdc8c19
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-04-30 12:03:47 +0100

    april 30th

--- pages/home/components/Intro.tsx
+++ pages/home/components/Intro.tsx
@@ -9,6 +9,7 @@ function Intro(props) {
             <div className={classes.Intro1}>
                 <div className={classes.mainIntro}>
                     <h1>Every Outfit <br/>For<br/> Every Event</h1>
+                    <p>We've got every style for every weather, event or mood you're in</p>
                 </div>
             </div>
         </div>
